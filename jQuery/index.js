d66008fe65b418ff663284f7a3a159ace5ad7d4c
yjtan0819 <tan.yijia0819@gmail.com>
2023-01-03 22:44:55 -0500

    Added website animations

--- jQuery/index.js
+++ jQuery/index.js
@@ -25,3 +25,15 @@ $(document).keypress(function (event) {
 $("h1").on("mouseover", function(){
     $("h1").css("color", "purple");
 });
+
+// $("button").on("click", function(){
+//     $("h1").toggle();
+// });
+
+$("button").on("click", function(){
+    $("h1").animate({opacity: 0.5});
+});
+
+$("button").on("click", function(){
+    $("h1").slideUp().slideDown().animate({opacity: 0.5});
+});
